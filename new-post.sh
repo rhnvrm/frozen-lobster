dc1d485c46f35c36adcbdf45e88ba665826a526a
Rohan Verma <lobster-bot@rohanverma.net>
2020-12-19 00:05:02 +0000

    post: new post for 2020-12-19

--- new-post.sh
+++ new-post.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 set -e
-hugo new posts/$(date '+%Y-%m-%d').md
+/usr/local/bin/hugo new posts/$(date '+%Y-%m-%d').md
 git add content/posts/$(date '+%Y-%m-%d').md
 git commit -m "post: new post for $(date '+%Y-%m-%d')"
 git push origin master
